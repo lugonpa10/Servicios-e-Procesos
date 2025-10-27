6570b6e1109884768d95ffa6b58f2df566817a7d
lugonpa10 <lugonpa10@gmail.com>
2025-10-27 13:59:44 +0100

    changes

---
+++ TemaHilos/PruebaTaskWhen/PruebaTaskWhen/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace PruebaTaskWhen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnColor = new System.Windows.Forms.Button();
+            this.btnAll = new System.Windows.Forms.Button();
+            this.btnAny = new System.Windows.Forms.Button();
+            this.txtResultados = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnColor
+            // 
+            this.btnColor.Location = new System.Drawing.Point(143, 56);
+            this.btnColor.Name = "btnColor";
+            this.btnColor.Size = new System.Drawing.Size(75, 23);
+            this.btnColor.TabIndex = 0;
+            this.btnColor.Text = "Color";
+            this.btnColor.UseVisualStyleBackColor = true;
+            this.btnColor.Click += new System.EventHandler(this.btnColor_Click);
+            // 
+            // btnAll
+            // 
+            this.btnAll.Location = new System.Drawing.Point(143, 163);
+            this.btnAll.Name = "btnAll";
+            this.btnAll.Size = new System.Drawing.Size(75, 23);
+            this.btnAll.TabIndex = 1;
+            this.btnAll.Text = "WhenAll";
+            this.btnAll.UseVisualStyleBackColor = true;
+            this.btnAll.Click += new System.EventHandler(this.btnAll_Click);
+            // 
+            // btnAny
+            // 
+            this.btnAny.Location = new System.Drawing.Point(143, 104);
+            this.btnAny.Name = "btnAny";
+            this.btnAny.Size = new System.Drawing.Size(75, 23);
+            this.btnAny.TabIndex = 2;
+            this.btnAny.Text = "WhenAny";
+            this.btnAny.UseVisualStyleBackColor = true;
+            // 
+            // txtResultados
+            // 
+            this.txtResultados.Location = new System.Drawing.Point(244, 79);
+            this.txtResultados.Multiline = true;
+            this.txtResultados.Name = "txtResultados";
+            this.txtResultados.Size = new System.Drawing.Size(417, 313);
+            this.txtResultados.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtResultados);
+            this.Controls.Add(this.btnAny);
+            this.Controls.Add(this.btnAll);
+            this.Controls.Add(this.btnColor);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnColor;
+        private System.Windows.Forms.Button btnAll;
+        private System.Windows.Forms.Button btnAny;
+        private System.Windows.Forms.TextBox txtResultados;
+    }
+}
+
---
+++ TemaHilos/PruebaTaskWhen/PruebaTaskWhen/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PruebaTaskWhen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int SlowFind(int num, byte[] vector)
+        {
+            Random g = new Random();
+            int position;
+            // Busqueda mediante índices aleatorios
+            // La velocidad depende de la suerte
+            do
+            {
+                position = g.Next(vector.Length);
+            } while (num != vector[position]);
+            return position;
+        }
+        public static int SlowFindSleep(int num, byte[] vector)
+        {
+            Random g = new Random();
+            int position = 0;
+            // Búsqueda lineal haciendo pausa (liberando la CPU)
+            // en cada vuelta del bucle
+            while (position < vector.Length && num != vector[position++])
+            {
+                Task.Delay(0).Wait();
+            }
+            return position;
+        }
+        // Genera vector con 100000000 de ceros y un 1 en una posición aleatoria
+        // Se realiza de tipo byte y no int para ocupar 100MB en lugar de 400MB
+        public byte[] generaVector()
+        {
+            Task.Delay(1).Wait(); // Evita mismo new Random() en sucesivas llamadas.
+            Random g = new Random();
+            byte[] v = new byte[100000000];
+            v[g.Next(v.Length)] = 1;
+            return v;
+        }
+
+        // Función que busca el 1 en todos los vectores
+        // de forma paralela usando WhenAll para la espera.
+        public async void InitAll()
+        {
+            txtResultados.Text = $"Buscando en todos los vectores";
+            btnAll.Enabled = false;
+            byte[] v1 = generaVector();
+            byte[] v2 = generaVector();
+            byte[] v3 = generaVector();
+            Task<int> task1 = Task.Run(() => SlowFind(1, v1));
+            Task<int> task2 = Task.Run(() => SlowFind(1, v2));
+            Task<int> task3 = Task.Run(() => SlowFind(1, v3));
+            // En esta línea se libera el bloqueo para hacer otras tareas
+            // y solo se continua cuando han acanbado las 3 tareas.
+            int[] tiempos = await Task.WhenAll(task1, task2, task3);
+            btnAll.Enabled = true;
+            txtResultados.Text = $"Resultados WhenAll{Environment.NewLine}";
+            for (int i = 0; i < tiempos.Length; i++)
+            {
+                txtResultados.AppendText($"Posición en v[{i}]: {tiempos[i]}" +
+                $"{Environment.NewLine}");
+            }
+        }
+
+
+        // Función que busca la posición del 1
+        // de un vector mediante el método más rápido
+        public async Task<string> InitAny()
+        {
+            txtResultados.Text = $"Buscando el método maás rápido";
+            Random g = new Random();
+            byte[] v = generaVector(); ;
+            Task<string> task1 = Task.Run(() => $"SlowFind: {SlowFind(1, v)}");
+            Task<string> task2 = Task.Run(() => $"SlowFindSleep: {SlowFindSleep(1, v)}");
+            // Si a IndexOf le pasas 1 sin casting busca un integer que no existe.
+            Task<string> task3 = Task.Run(() => $"IndexOf: {Array.IndexOf(v, (byte)1)}");
+            // Hay doble await porque WhenAny es del tipo Task<Task<string>>
+            // - Task interior: representa "la tarea que terminó primero"
+            // - Task exterior: representa "El dato que contiene esa tarea"
+            Task<string> tareaFinal = await Task.WhenAny(task3, task2, task1);
+            return await tareaFinal;
+        }
+        private async void btnAny_Click(object sender, EventArgs e)
+        {
+            string ganador = await InitAny();
+            txtResultados.Text = $"Resultados WhenAny{Environment.NewLine}{ganador}";
+        }
+
+
+        private void btnColor_Click(object sender, EventArgs e)
+        {
+            Random g = new Random();
+            this.BackColor = Color.FromArgb(255, g.Next(256), g.Next(256), g.Next(256));
+        }
+
+        private void btnAll_Click(object sender, EventArgs e)
+        {
+            InitAll();
+        }
+    }
+}
