f303c2f75f864db71c06f7a5cd141a3af398eaae
lugonpa10 <lugonpa10@gmail.com>
2025-10-29 11:46:50 +0100

    changes

--- Tema4/Ejercicio5/Ejercicio5/Program.cs
+++ Tema4/Ejercicio5/Ejercicio5/Program.cs
@@ -6,14 +6,63 @@
         public delegate void MyDelegate();
         static void Main(string[] args)
         {
-            Console.WriteLine("por favor que se guarden los cambios");
-            Console.WriteLine("hola");
-            Console.WriteLine("GUARDAR CAMBIOS GITHUB");
-            string odioGit = "no puedo mas";
+            string[] opciones = { "A", "B", "C", "Exit" };
+            MenuGenerator(opciones);
+
         }
 
 
-        public static void MenuGenerator(string[] opciones, MyDelegate[] functions) { }
+        public static void MenuGenerator(string[] opciones)
+        {
+
+            int opcion = 0;
+
+            do
+            {
+                Console.WriteLine("Introduce una opcion");
+                opcion = int.Parse(Console.ReadLine());
+                for (int i = 0; i < opciones.Length; i++)
+                {
+                    Console.WriteLine(opciones[i]);
+                }
+
+
+                switch (opcion)
+
+                {
+                    case 1:
+
+                        static void f1()
+                        {
+                            Console.WriteLine("A");
+                        }
+                        break;
+
+                    case 2:
+
+                        static void f2()
+                        {
+                            Console.WriteLine("B");
+                        }
+                        break;
+
+                    case 3:
+
+                        static void f3()
+                        {
+                            Console.WriteLine("C");
+                        }
+                        break;
+                }
+            }
+            while (opcion != 4);
+
+
+
+
+        }
+
+
 
     }
 }
