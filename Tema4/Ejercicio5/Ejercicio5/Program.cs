5ba8869b2e052489914cf5040e875dd07044bb55
lugonpa10 <lugonpa10@gmail.com>
2025-10-30 10:01:39 +0100

    changes

--- Tema4/Ejercicio5/Ejercicio5/Program.cs
+++ Tema4/Ejercicio5/Ejercicio5/Program.cs
@@ -6,60 +6,59 @@
         public delegate void MyDelegate();
         static void Main(string[] args)
         {
-            string[] opciones = { "A", "B", "C", "Exit" };
-            MenuGenerator(opciones);
+            //string[] opciones = { "A", "B", "C", "Exit" };
+            PedirEnteros(1, 4);
+            MenuGenerator(new string[] { "Op1", "Op2", "Op3" }, new MyDelegate[] { f1, f2, f3 });
+            Console.ReadKey();
+
 
         }
 
 
-        public static void MenuGenerator(string[] opciones)
+        public static void MenuGenerator(string[] opciones, MyDelegate[] delegado)
         {
 
-            int opcion = 0;
+            for (int i = 0; i < opciones.Length; i++)
+            {
+                Console.WriteLine(opciones[i]);
+            }
+            static void f1()
+            {
+                Console.WriteLine("A");
+            }
+            static void f2()
+            {
+                Console.WriteLine("B");
+            }
+            static void f3()
+            {
+                Console.WriteLine("C");
+            }
+        }
+
+        public static int PedirEnteros(int min, int max)
+        {
+            int num;
+            bool flag = true;
 
             do
             {
-                Console.WriteLine("Introduce una opcion");
-                opcion = int.Parse(Console.ReadLine());
-                for (int i = 0; i < opciones.Length; i++)
+                Console.WriteLine($"Introduce una opcion entre {min} y {max}");
+                flag = int.TryParse(Console.ReadLine(), out num);
+                if (!flag)
                 {
-                    Console.WriteLine(opciones[i]);
+                    Console.WriteLine("Argumento invalido");
+
                 }
-
-
-                switch (opcion)
-
+                else if (num < min || num > max)
                 {
-                    case 1:
-
-                        static void f1()
-                        {
-                            Console.WriteLine("A");
-                        }
-                        break;
-
-                    case 2:
-
-                        static void f2()
-                        {
-                            Console.WriteLine("B");
-                        }
-                        break;
-
-                    case 3:
-
-                        static void f3()
-                        {
-                            Console.WriteLine("C");
-                        }
-                        break;
+                    Console.WriteLine("Numero fuera de rango");
                 }
             }
-            while (opcion != 4);
+            while (!flag || (num < min || num > max));
 
 
-
-
+            return num;
         }
 
 
