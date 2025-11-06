3809c63810603294c6d10bae9a52278c6a8f2f90
lugonpa10 <lugonpa10@gmail.com>
2025-11-06 23:08:32 +0100

    changes

--- BolHilos/Ejercicio1/Ejercicio1/Program.cs
+++ BolHilos/Ejercicio1/Ejercicio1/Program.cs
@@ -2,9 +2,76 @@
 {
     internal class Program
     {
+        static int cont = 0;
+        static bool flag = true;
+        static readonly object l = new object();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Thread th1 = new Thread(() =>
+                {
+
+                    while (flag)
+                    {
+
+                        lock (l)
+                        {
+                            if (!flag)
+                            {
+                                break;
+                            }
+                            cont++;
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine($"Hilo 1: {cont}");
+                            Console.ResetColor();
+
+                            if (cont >= 500)
+                            {
+                                flag = false;
+                                break;
+                            }
+                        }
+                    }
+                });
+
+            Thread th2 = new Thread(() =>
+            {
+                while (flag)
+                {
+                    lock (l)
+                    {
+                        if (!flag)
+                        {
+                            break;
+                        }
+                        cont--;
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine($"Hilo 2: {cont}");
+                        Console.ResetColor();
+                        if (cont <= -500)
+                        {
+                            flag = false;
+                            break;
+                        }
+
+
+                    }
+
+
+                }
+            });
+            th1.Start();
+            th2.Start();
+            //th1.Join();
+            th2.Join();
+            if (cont >= 500)
+            {
+                Console.WriteLine("Ganador hilo 1");
+            }
+            else if (cont <= -500)
+            {
+                Console.WriteLine("Ganador hilo 2");
+            }
         }
     }
 }
+
