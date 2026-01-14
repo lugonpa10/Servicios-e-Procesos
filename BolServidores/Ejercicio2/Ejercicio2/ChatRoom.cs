6e404b89710dd7aebebd8827c8a363891073fe44
lugonpa10 <lugonpa10@gmail.com>
2026-01-14 11:52:59 +0100

    changes

---
+++ BolServidores/Ejercicio2/Ejercicio2/ChatRoom.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    internal class ChatRoom
+    {
+        public bool serverRunning = true;
+        public int Port { set; get; } = 31416;
+        public int[] puertosAlternativos = { 135, 135, 135, 2324 };
+        public bool puertoOcupado = true;
+        private Socket s;
+        private int i;
+     
+
+        public void initServer()
+        {
+            IPEndPoint ie = new IPEndPoint(IPAddress.Any, Port);
+            s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                while (puertoOcupado)
+                {
+                    try
+                    {
+                    puertoOcupado = false;
+                    s.Bind(ie);
+
+                    }catch(SocketException e) when (e.ErrorCode == 10048)
+                    {
+                        puertoOcupado = true;
+                        ie.Port = puertosAlternativos[i];
+                        i++;
+                    }
+                }
+                Console.WriteLine($"El puerto {ie.Port} esta libre");
+                s.Listen(10);
+
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Puertos ocupados");
+            }catch(SocketException e) when (e.ErrorCode == 10048)
+            {
+                Console.WriteLine($"Puerto ");
+            }
+            catch (SocketException)
+            {
+                Console.WriteLine("Fin del servidor");
+            }
+
+
+        }
+
+
+
+    }
+}
---
+++ BolServidores/Ejercicio2/Ejercicio2/Cliente.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    internal class Cliente
+    {
+        private string nombreUsuario;
+        private int ip;
+
+        public string NombreUsuario
+        {
+            set
+            {
+                nombreUsuario = value;
+            }
+            get
+            {
+                return nombreUsuario;
+            }
+        }
+
+        public int Ip
+        {
+            set
+            {
+                ip = value;
+            }
+            get
+            {
+                return ip;
+            }
+        }
+    }
+}
