bc10f0a4d763321a9801b386cc0bc6d31887a8d0
lugonpa10 <lugonpa10@gmail.com>
2026-01-22 12:28:45 +0100

    changes

---
+++ BolServidores/Ejercicio3/Ejercicio3/ShiftServer.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio3
+{
+    internal class ShiftServer
+    {
+        public string[] users;
+        public List<string> waitQueue = new List<string>();
+        public int Port { set; get; } = 31416;
+        public int puertoReferencia = 1024;
+        private Socket s;
+        public bool puertoOcupado = true;
+        public bool ServeRunning { set; get; } = true;
+
+        public void ReadNames(string rutaArchivo)
+        {
+            try
+            {
+                string linea = "";
+                using (StreamReader sr = new StreamReader(rutaArchivo)) // TODO varias lineas.
+                {
+                    while ((linea = sr.ReadLine()) != null)
+                    {
+                        users = linea.Split(";");
+
+                    }
+                }
+
+
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("Error con el archivo");
+            }
+        }
+
+        public int ReadPin(string rutaArchivo)
+        {
+            try
+            {
+                string linea = "";
+                string pinTexto;
+                int pinNumeros;
+
+                using (StreamReader sr = new StreamReader(rutaArchivo))
+                {
+                    linea = sr.ReadLine();
+                    if (linea.Length != 4 || linea == null)
+                    {
+                        return -1;
+
+                    }
+                    else
+                    {
+                        pinTexto = linea.Substring(0, 4);
+                        if (!int.TryParse(pinTexto, out pinNumeros))
+                        {
+                            return -1;
+
+                        }
+                        else
+                        {
+                            return pinNumeros;
+
+                        }
+
+
+                    }
+                }
+            }
+            catch (IOException)
+            {
+                return -1;
+            }
+        }
+
+        public void Init()
+        {
+            IPEndPoint ie = new IPEndPoint(IPAddress.Any, Port);
+            s = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                while (puertoOcupado)
+                {
+                    try
+                    {
+                        puertoOcupado = false;
+                        s.Bind(ie);
+
+                    }
+                    catch (SocketException e) when (e.ErrorCode == 10048)
+                    {
+                        puertoOcupado = true;
+                        ie.Port = puertoReferencia;
+                        puertoReferencia++;
+
+                    }
+                }
+
+                Console.WriteLine($"El puerto {Port} esta a la escucha");
+                while (ServeRunning)
+                {
+
+                    
+                }
+
+            }
+            catch (SocketException e) when (e.ErrorCode == 10048)
+            {
+                Console.WriteLine($"El puerto {Port} esta ocupado");
+
+            }
+            catch (SocketException)
+            {
+                Console.WriteLine("Fin del servidor");
+            }
+           
+
+        }
+
+    }
+}
